545a1909376a643eb04ebed99b1105b5c8a52231
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-05-06 12:49:55 -0500

    first commit

---
+++ src/net/idscan/m280sampleapp/Main.java
@@ -0,0 +1,41 @@
+package net.idscan.m280sampleapp;
+
+import com.fazecast.jSerialComm.*;
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	
+	 public static void main(String[] args) {
+		 
+		 SerialPort comPort = SerialPort.getCommPorts()[0];
+		 comPort.openPort();
+		 
+		 try {
+		    while (true)
+		    {
+		    	
+		       while (comPort.bytesAvailable() == 0) {
+		    	   Thread.sleep(1000);
+		       }
+	
+		       byte[] readBuffer = new byte[comPort.bytesAvailable()];
+		       int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+		       String str = new String(readBuffer);
+		       System.out.println(str);
+		       
+		       
+		    }
+		 } 
+		 catch (Exception e) { 
+			 e.printStackTrace(); 
+		 }
+		 
+		 comPort.closePort();
+	 }
+}
+
+
+
+
