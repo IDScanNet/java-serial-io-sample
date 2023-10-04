92cb5d01b433ff7fb34982b0b658dda0566da581
Cary Grant II <90705075+IDScanCaryGrantII@users.noreply.github.com>
2023-10-04 10:50:34 -0500

    Update Main.java
    Adding loop to show COM ports

--- src/net/idscan/m280sampleapp/Main.java
+++ src/net/idscan/m280sampleapp/Main.java
@@ -8,7 +8,11 @@ import java.io.*;
 public class Main {
 	
 	 public static void main(String[] args) {
-		 
+
+
+	 for (SerialPort port: SerialPort.getCommPorts()) {
+		System.out.println((port.getSystemPortName()));
+	}
 		 SerialPort comPort = SerialPort.getCommPorts()[0];
 		 comPort.openPort();
 		 System.out.println((comPort.getSystemPortName()));
